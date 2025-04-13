a1e94e01741bd242068e9935d46200ca5d651a3b
Gabriel432135 <gabriel432135@hotmail.com>
2025-04-13 17:22:12 -0300

    Funcionalidade dos botões

---
+++ app/src/main/java/com/example/artworkcompose/ArtworkItem.kt
@@ -0,0 +1,4 @@
+package com.example.artworkcompose
+
+class ArtworkItem(val picture: Int, val title: Int, val autor: Int, val data: Int) {
+}
--- app/src/main/java/com/example/artworkcompose/MainActivity.kt
+++ app/src/main/java/com/example/artworkcompose/MainActivity.kt
@@ -19,6 +19,10 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
@@ -53,7 +57,34 @@ fun LayoutWithBackground(){
 
 @Composable
 fun DisplayArtworkLayout(modifier: Modifier = Modifier) {
-    var image: Painter = painterResource(R.drawable.surrealismo)
+    val artworks = remember {
+        listOf(
+            ArtworkItem(
+                picture = R.drawable.surrealismo,
+                title = R.string.surrealismo_nome,
+                autor = R.string.surrealismo_autor,
+                data = R.string.surrealismo_data
+            ),
+            ArtworkItem(
+                picture = R.drawable.realista,
+                title = R.string.realista_nome,
+                autor = R.string.realista_autor,
+                data = R.string.realista_data
+            ),
+            ArtworkItem(
+                picture = R.drawable.desenho,
+                title = R.string.desenho_nome,
+                autor = R.string.desenho_autor,
+                data = R.string.desenho_data
+            )
+        )
+    }
+
+    var index: Int by remember{ mutableIntStateOf(0) }
+
+
+
+    val image: Painter = painterResource(artworks[index].picture)
     Column(modifier = Modifier.padding(10.dp, 10.dp).fillMaxSize().verticalScroll(rememberScrollState()), verticalArrangement = Arrangement.Center){
         Surface (modifier = Modifier
             .fillMaxWidth()
@@ -65,7 +96,7 @@ fun DisplayArtworkLayout(modifier: Modifier = Modifier) {
             Column(modifier = Modifier) {
                 Row(modifier = modifier, verticalAlignment = Alignment.CenterVertically) {
                     Text(
-                        text = "Artwork Title",
+                        text = stringResource(artworks[index].title),
                         modifier = modifier.fillMaxWidth(),
                         fontSize = 30.sp,
                         fontWeight = FontWeight.Light,
@@ -73,12 +104,13 @@ fun DisplayArtworkLayout(modifier: Modifier = Modifier) {
                 }
                 Row(modifier = modifier, verticalAlignment = Alignment.CenterVertically) {
                     Text(
-                        text = "Autor",
+                        text =  stringResource(artworks.get(index).autor),
                         fontSize = 15.sp,
                         fontWeight = FontWeight.Black,
                     )
+                    Spacer(modifier = Modifier.padding(2.dp))
                     Text(
-                        text = "(Data)",
+                        text = String.format("(%s)",stringResource(artworks[index].data)),
                         fontSize = 15.sp,
                         fontWeight = FontWeight.Light,
                     )
@@ -94,14 +126,14 @@ fun DisplayArtworkLayout(modifier: Modifier = Modifier) {
             horizontalArrangement = Arrangement.Center
             )
         {
-            Button(onClick = {}){
+            Button(onClick = {index = (index + 1 + artworks.size)%artworks.size}){
                 Text(
                     text = stringResource(R.string.previous_button),
                     fontSize = 15.sp,
                 )
             }
             Spacer(modifier = Modifier.padding(10.dp))
-            Button(onClick = {}){
+            Button(onClick = {index = (index - 1 + artworks.size)%artworks.size}){
                 Text(
                     text = stringResource(R.string.next_button),
                     fontSize = 15.sp,
